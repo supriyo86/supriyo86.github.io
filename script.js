472fc639a92fb8619824512d538ca31f13837d1e
supriyo86 <supriyo.talukder86@gmail.com>
2026-03-21 15:24:43 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const themeToggle = document.getElementById("themeToggle");
 
+themeToggle.addEventListener("click", () => {
+  document.body.classList.toggle("light-mode");
+
+  if (document.body.classList.contains("light-mode")) {
+    themeToggle.textContent = "☾";
+  } else {
+    themeToggle.textContent = "☀";
+  }
+});
