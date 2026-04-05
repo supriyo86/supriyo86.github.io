41be4d67abcb9ae2105fd7236cb6973bfc9c74ef
supriyo86 <supriyo.talukder86@gmail.com>
2026-04-04 19:37:59 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-const toggle = document.getElementById("themeToggle");
-
-toggle.addEventListener("click", () => {
-  document.body.classList.toggle("light-mode");
-
-  if (document.body.classList.contains("light-mode")) {
-    toggle.textContent = "☾";
-  } else {
-    toggle.textContent = "☀";
-  }
-});
+console.log("Portfolio Loaded");
