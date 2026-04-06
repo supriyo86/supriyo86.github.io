1988ec9a65dbca6cd38a7fec539adc9216e70e83
supriyo86 <supriyo.talukder86@gmail.com>
2026-04-05 22:57:05 -0700

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -1,1 +1,8 @@
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.getElementById("navLinks");
 
+if (menuToggle && navLinks) {
+  menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+  });
+}
