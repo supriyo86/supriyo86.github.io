26568af250b99995c7fa1ffc2b33ad67904395f7
supriyo86 <supriyo.talukder86@gmail.com>
2026-05-16 15:45:56 -0700

    Delete js directory

--- js/footer.js
+++
@@ -1,5 +0,0 @@
-const footerYear = document.getElementById("footer-year");
-
-if (footerYear) {
-  footerYear.textContent = new Date().getFullYear();
-}
--- js/navbar.js
+++
@@ -1,8 +0,0 @@
-const menuToggle = document.getElementById("menuToggle");
-const navLinks = document.getElementById("navLinks");
-
-if (menuToggle && navLinks) {
-  menuToggle.addEventListener("click", () => {
-    navLinks.classList.toggle("show");
-  });
-}
